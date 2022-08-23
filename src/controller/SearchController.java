09adb7c5c13eccee1be055ed8a9d4a2d4e3daac3
bahastn <bahastn@yahoo.com>
2022-08-23 10:21:39 +0300

    comit_23

--- src/controller/SearchController.java
+++ src/controller/SearchController.java
@@ -1,9 +1,15 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class SearchController {
 
 
+    public void searchEmployee(ActionEvent actionEvent) {
+    }
 
+    public void printPriview(MouseEvent mouseEvent) {
+    }
 }
